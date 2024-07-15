63308758c10805e6f54e7496bff4c98847361e55
FortGrace <g.a.l.a91@mail.ru>
2024-07-15 23:34:32 +0700

    Complete project. User, UserDaoJDBCImpl, UserServiceImpl, Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/test";
     private static final String NAME_USER = "root";
     private static final String PASSWORD = "123456";
 
@@ -15,14 +15,6 @@ public class Util {
             connection = DriverManager.getConnection(URL, NAME_USER, PASSWORD);
         } catch (SQLException trowables) {
             trowables.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException trowables) {
-                trowables.printStackTrace();
-            }
         }
         return connection;
     }
